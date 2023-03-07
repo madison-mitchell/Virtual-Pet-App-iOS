2e34336c60b585c65a326a75c563b99453016236
Madison Mitchell <madisonrosemitchell@gmail.com>
2023-03-07 09:53:43 -0500

    Initial Commit

---
+++ Virtual-Pet_App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Virtual-Pet_App
+//
+//  Created by Madison Mitchell on 3/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Virtual-Pet_App/Virtual_Pet_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_Pet_AppApp.swift
+//  Virtual-Pet_App
+//
+//  Created by Madison Mitchell on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_Pet_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
